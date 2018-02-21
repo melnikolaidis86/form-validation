1829a6fa2035bee4400770919e771100234ee9e6
Meletis Nikolaidis <melnikolaidis@hotmail.com>
2018-02-21 16:09:38 +0200

    Fixing scripts

--- app/assets/scripts/validation.js
+++ app/assets/scripts/validation.js
@@ -1,72 +1,36 @@
-form = document.getElementById('form');
-
-form.fullName.onblur = function() {
+function Validation (validationField, validationRules = {required: false, minLength: 0, maxLength: 0, regex: false}) {
     
-    nameValidation = new Validation('ονοματεπώνυμο', form.fullName.value); 
-    nameValidation.validateMinLength(3);
-    nameValidation.validateMaxLength(30);
-    nameValidation.displayErrors('fullNameError');
-}
-
-form.email.onblur = function() {
+    this.field = validationField;
+    this.rules = validationRules;
     
-    emailValidation = new Validation('Ε-mail', form.email.value);
-    emailValidation.validateRegex(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/);
-    emailValidation.displayErrors('emailError');
-}
-
-form.email2.onblur = function() {
-    
-    email = form.email.value;
-    emailVerification = new Validation('E-mail', form.email2.value);
-    emailVerification.validateEqual('E-mail', email);
-    emailVerification.displayErrors('email2Error');
-}
-
-
-function Validation (inputField, fieldValue) {
-    
-    this.input = inputField;
-    this.value = fieldValue;
-    
-    this.errors = new Array();
-    
-    this.validateMinLength = function (minLength) {
+    this.validate = function () {
         
-        if(this.value.length < minLength) {
+        var errors = {};
+        
+        if(this.rules.required == true && this.field == "") {
+            errors.fieldRequiredError = "This field is required";
+        }
+        
+        if(this.rules.minLength != 0 && this.field.length < this.rules.minLength) {
+            errors.minLengthError = "Give more chars";
+        }
+        
+        if(this.rules.maxLength != 0 && this.field.length > this.rules.maxLength) {
+            errors.maxLengthError = "Too many chars";
+        }
+        
+        if(this.rules.regex != false) {
+            var regexp = RegExp(this.regex);
+            var testField = this.field;
             
-            this.errors.push('Παρακαλούμε, για το πεδίο ' + this.input + ' απαιτούνται τουλάχιστον ' + minLength + ' χαρακτήρες.');
+            if(!regexp.test(testField)) {
+                
+                errors.regexError = "Enter a valid string";
+            }
         }
-    }
-       
-    this.validateMaxLength = function (maxLength) {
         
-        if(this.value.length > maxLength) {
-            
-            this.errors.push('Παρακαλούμε, για το πεδίο ' + this.input + ' επιτρέπονται το πολύ ' + maxLength + ' χαρακτήρες.');
-        }
+        return errors;
+    
     }
     
-    this.validateEqual = function (inputFieldEqual, inputValueEqual) {
-        
-        if(this.value != inputValueEqual) {
-            
-            this.errors.push('Tο πεδίο ' + this.input + ' δεν ταιριάζει με το πεδίο ' + inputFieldEqual + '.');
-        }
-    }
-    
-    this.validateRegex = function (regex) {
-        
-        if(!regex.test(this.value)) {
-            
-            this.errors.push('Παρακαλούμε εισάγετε μια έγκυρη μορφή για το πεδίο ' + this.input + '.');
-        }
-    }
-    
-    this.displayErrors = function (errorId) {
-        
-        for (var key in this.errors) {
-            document.getElementById(errorId).innerHTML = this.errors[key] + ' ';  
-        }
-    }
 }
